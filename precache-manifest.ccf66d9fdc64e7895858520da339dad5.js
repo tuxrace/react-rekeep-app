4592c4bcfbde16b6836c122b2a1a88783d33859c
Gerard Banasig <tuxrace@yahoo.com>
2020-05-18 01:14:51 +0800

    Updates

---
+++ precache-manifest.ccf66d9fdc64e7895858520da339dad5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a31906c9f97a0553331f5f26e8c2450",
+    "url": "/react-rekeep-app/index.html"
+  },
+  {
+    "revision": "0d301139f6d5d49e437e",
+    "url": "/react-rekeep-app/static/js/2.7c68053a.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "/react-rekeep-app/static/js/2.7c68053a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2c1b4afe91f1b963dd",
+    "url": "/react-rekeep-app/static/js/main.7a770560.chunk.js"
+  },
+  {
+    "revision": "6d7d5c3a49cf9d7cf0db",
+    "url": "/react-rekeep-app/static/js/runtime-main.63715e25.js"
+  }
+]);
