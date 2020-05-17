dc60fb82378532cffafc9e00117229d9b3f02082
Gerard Banasig <tuxrace@yahoo.com>
2020-05-18 01:28:23 +0800

    Use Hash Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import CreatePart from './pages/CreatePart';
 import Home from './pages/Home';
 import Header from './components/Header';
