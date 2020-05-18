9518a5f6ae7bfa9abfd2868a41833492d722c944
Gerard Banasig <tuxrace@yahoo.com>
2020-05-18 18:17:53 +0800

    Link styling enhancements

--- src/components/Header.styles.ts
+++ src/components/Header.styles.ts
@@ -3,6 +3,9 @@ import { Theme } from '@material-ui/core/styles';
 export default (theme: Theme) => ({
     root: {
       flexGrow: 1,
+      '& a:-webkit-any-link': {
+        textDecoration: 'none'
+      }
     },
     title: {
       flexGrow: 1,
